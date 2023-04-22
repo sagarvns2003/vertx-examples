89b7f9f83607e2b5e00b734070a8f31ef3905621
Vidya Sagar Gupta <v3sagar@gmail.com>
2023-04-23 00:33:08 +0530

    Vertx first and simple example

---
+++ first-demo/src/main/java/io/github/sagarvns2003/example/MainVerticle.java
@@ -0,0 +1,24 @@
+package io.github.sagarvns2003.example;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Promise<Void> startPromise) throws Exception {
+		vertx.createHttpServer().requestHandler(req -> {
+			req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+		}).listen(8888, http -> {
+			if (http.succeeded()) {
+				startPromise.complete();
+				System.out.println("HTTP server started on port 8888");
+				System.out.println("HTTP URL: http://localhost:8888/");
+			} else {
+				startPromise.fail(http.cause());
+			}
+		});
+	}
+
+	
+}
